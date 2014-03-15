263c6c3c8c9bcc27b0465a873567562edf984d07
Michael Thelin <thelinmichael@spotify.com>
2014-03-15 01:24:17 +0100

    Changed test to make more sense

--- test/smhi-test.js
+++ test/smhi-test.js
@@ -4,20 +4,22 @@ var should = require("should"),
 describe("SMHI", function() {
 
   it("should fail if the response is not valid for a valid request", function(done) {
-    var latitude = 58.59;
-    var longitude = 16.18;
+    var latitude = 58.59,
+        longitude = 16.18;
 
     unit.getForecastForLatAndLong(latitude, longitude).then(
       function(response) {
-        isValidResponse(response).should.equal(true);
+        var forecasts = response.getForecasts();
+        should.exist(forecasts);
+        should.exist(forecasts.length);
+        forecasts.length.should.not.equal(0);
+        forecasts[0].getLatitude().should.equal(58.548703); // closest node
+        forecasts[0].getLongitude().should.equal(16.155116); // closest node
         done();
       },
       function(error) {
         done(error);
       });
   });
-});
 
-var isValidResponse = function(response) {
-  return true;
-};
+});
